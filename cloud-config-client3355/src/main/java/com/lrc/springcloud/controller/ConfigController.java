c514c4fca9f0466974105035908a504ca7a555c9
lrc <1477959747@qq.com>
2021-08-11 22:17:15 +0800

    ...

--- cloud-config-client3355/src/main/java/com/lrc/springcloud/controller/ConfigController.java
+++ cloud-config-client3355/src/main/java/com/lrc/springcloud/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package com.lrc.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/8/11 21:52
  */
 @RestController
+@RefreshScope
 public class ConfigController {
 
-    @Value("${config}")
+    @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/configInfo")
